573dcfe277f895327c2ccf7fc81e39deae299745
Kunmsey <akinlabiakinkunmi100@gmail.com>
2022-07-21 00:48:25 +0100

    adding area.py

---
+++ area.py
@@ -0,0 +1,27 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python39/area.py =====
+Traceback (most recent call last):
+  File "C:/Users/HP/AppData/Local/Programs/Python/Python39/area.py", line 1, in <module>
+    from area import area_of_triangle
+  File "C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py", line 1, in <module>
+    from area import area_of_triangle
+ImportError: cannot import name 'area_of_triangle' from partially initialized module 'area' (most likely due to a circular import) (C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py)
+>>> 
+====== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python39/area.py =====
+Traceback (most recent call last):
+  File "C:/Users/HP/AppData/Local/Programs/Python/Python39/area.py", line 1, in <module>
+    from area import area_of_triangle
+  File "C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py", line 1, in <module>
+    from area import area_of_triangle
+ImportError: cannot import name 'area_of_triangle' from partially initialized module 'area' (most likely due to a circular import) (C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py)
+>>> 
+=== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python39/classfile.py ===
+Traceback (most recent call last):
+  File "C:/Users/HP/AppData/Local/Programs/Python/Python39/classfile.py", line 1, in <module>
+    from area import area_of_triangle
+  File "C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py", line 1, in <module>
+    from area import area_of_triangle
+ImportError: cannot import name 'area_of_triangle' from partially initialized module 'area' (most likely due to a circular import) (C:\Users/HP/AppData/Local/Programs/Python/Python39\area.py)
+>>> 
